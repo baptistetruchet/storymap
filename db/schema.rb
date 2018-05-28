bf915f65f86afa60b4ae9ee3860ac6366d8c9fd2
Baptiste Truchet <baptiste.truchet@gmail.com>
2018-05-28 15:19:42 +0200

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_115709) do
+ActiveRecord::Schema.define(version: 2018_05_28_121025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_05_28_115709) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
     t.index ["block_id"], name: "index_events_on_block_id"
   end
 
