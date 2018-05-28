548c587c22c133ad7b3dca3bc104a177ee8a2e8e
Baptiste Boucher <baptiste.boucher@gmail.com>
2018-05-28 14:44:52 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users, except: [:index]
+  resources :stories do
+    get 'published', on: :member
+    resources :blocks, only: [:new, :create]
+  end
+  resources :blocks, only: [:show, :edit, :update, :destroy] do
+    resources :events, only: [:new, :create]
+  end
+  resources :events, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
