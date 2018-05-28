37db67f7680cdaec60ced11fbd3ca3dc3faa7990
Baptiste Truchet <baptiste.truchet@gmail.com>
2018-05-28 15:54:43 +0200

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 2018_05_28_132700) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
     t.float "latitude"
     t.float "longitude"
-    t.string "photo_url"
     t.index ["block_id"], name: "index_events_on_block_id"
   end
 
