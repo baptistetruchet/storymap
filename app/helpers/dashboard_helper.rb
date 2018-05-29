fc304d127c4c206feacd75f371e4ebed392be0b4
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-05-29 19:41:07 +0200

    fixed picture issue

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,7 +3,7 @@ module DashboardHelper
     story_photo = 'https://images.unsplash.com/photo-1497302347632-904729bc24aa?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=144d48440ee329f1d475ac5db69d66fc&auto=format&fit=crop&w=1650&q=80'
     unless story.blocks.empty?
       story.blocks.first.events.each do |event|
-        if event.photo_url
+        unless event.photo_url == ''
           story_photo = event.photo_url
         end
       end
