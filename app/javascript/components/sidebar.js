7ce2f702351be784a6f3b7e60f7a9a1c166ca497
Baptiste Boucher <baptiste.boucher@gmail.com>
2018-05-30 12:45:09 +0200

    create timeline with the right number of events and show with the right block

--- app/javascript/components/sidebar.js
+++ app/javascript/components/sidebar.js
@@ -1,3 +1,15 @@
+function setTimelineVisibility(position) {
+  let timelines = document.querySelectorAll('#timeline');
+  timelines.forEach((timeline) => {
+    let block_id = timeline.getAttribute("block-id");
+    if (parseInt(block_id, 10) === position) {
+      timeline.classList.remove("hidden-timeline");
+    } else {
+      timeline.classList.add("hidden-timeline");
+    }
+  });
+}
+
 function setDataAttribute(target) {
   let indexTarget = target.dataset.index;
   let blocks = document.querySelectorAll('.block');
@@ -10,6 +22,7 @@ function scrollTo(target) {
   let position = Array.from(target.parentNode.children).indexOf(target);
   let wrapper = document.getElementById('wrapper-blocks');
   wrapper.style.transform = `translate3d(0px, -${position * 44.5}px, 0px)`;
+  setTimelineVisibility(position);
 }
 
 function addListener(event) {
@@ -41,3 +54,5 @@ function rolling() {
 }
 
 export { rolling };
+
+
