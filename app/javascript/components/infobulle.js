9a97ce70f1a50aedbf9b1d2d2118d90607fd70d1
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-06-01 15:05:30 +0200

    working on it

--- app/javascript/components/infobulle.js
+++ app/javascript/components/infobulle.js
@@ -1,52 +1,13 @@
-function getPreviousSiblings(el, filter) {
-  var siblings = [];
-  while (el = el.previousSibling) { if (!filter || filter(el)) siblings.push(el); }
-  return siblings;
-}
-
-function getNextSiblings(el, filter) {
-  var siblings = [];
-  while (el= el.nextSibling) { if (!filter || filter(el)) siblings.push(el); }
-  return siblings;
-}
-
-function flow() {
-  let hist_events = document.querySelectorAll('.event');
-  hist_events.forEach((hist_event) => {
-    hist_event.addEventListener("click", (event) => {
-      let previousSiblings = getPreviousSiblings(hist_event, false)
-        previousSiblings.forEach((previousSibling) => {
-          if (previousSibling.tagName === 'DIV') previousSibling.classList.add("opacity-none");
-        });
-      let nextSiblings = getNextSiblings(hist_event, false)
-        nextSiblings.forEach((nextSibling) => {
-          if (nextSibling.tagName === 'DIV') nextSibling.classList.remove("opacity-none");
-        });
-      hist_event.className += " opacity-none";
-    });
+function setEventVisibility(eventid) {
+  let events = document.querySelectorAll('.infobulle');
+  events.forEach((event) => {
+    let block_id = timeline.getAttribute("block-id");
+    if (parseInt(block_id, 10) === position) {
+      timeline.classList.remove("hidden-timeline");
+    } else {
+      timeline.classList.add("hidden-timeline");
+    }
   });
 }
 
-function nextBlock() {
-  let timelines = document.querySelectorAll('#timeline');
-  let last_pins = document.querySelectorAll('.end');
-  last_pins.forEach((last_pin) => {
-    timelines.forEach((timeline) => {
-      if (timeline.className != "hidden-timeline") {
-        last_pin.addEventListener("click", (event) => {
-          let blocks = document.querySelectorAll('.block');
-          var selected_block = undefined;
-          blocks.forEach((block) => {
-            if (parseInt(block.dataset.index) === 1) {
-              selected_block = block;
-            }
-          });
-          console.log(selected_block);
-          selected_block.click();
-        });
-      }
-    });
-  });
-}
-
-export { flow, nextBlock };
+export { setEventVisibility };
