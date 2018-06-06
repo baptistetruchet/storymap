25ef6efbe8a4066e1ddbc3590551b9d712b3538b
Shiraz Kittelberger-Buehlbecker <buehlbeckershiraz@gmail.com>
2018-06-06 16:48:18 +0200

    finished blocks and events keyboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,13 +10,13 @@ import { rolling } from '../components/sidebar';
 const sidebar = document.getElementById('wrapper-blocks');
 if (sidebar) { rolling(); }
 
-import { flow, nextBlock, navigateBlocks, initNavigation } from '../components/timeline';
+import { flow, nextBlock, initNavigation, initNavigationEvents } from '../components/timeline';
 const timeline = document.getElementById('timeline');
 if (timeline) {
   flow();
   nextBlock();
   initNavigation();
-  navigateBlocks();
+  initNavigationEvents();
 }
 
 import { toggleBlock } from '../components/toggle_block';
