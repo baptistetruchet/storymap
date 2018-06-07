a4e2e15854997d6f8f2a35294ceb1a78ca6855d8
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-06-07 16:08:24 +0200

    finished

--- app/javascript/components/mapzone.js
+++ app/javascript/components/mapzone.js
@@ -1,19 +1,20 @@
 function initializemapzone() {
   // Map Center
-  var myLatLng = new google.maps.LatLng(33.5190755, -111.9253654);
+  var myLatLng = new google.maps.LatLng(52.2296756, 21.0122287);
   // General Options
   var mapOptions = {
-    zoom: 12,
+    zoom: 5,
     center: myLatLng,
     mapTypeId: google.maps.MapTypeId.RoadMap
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
   // Polygon Coordinates
   var triangleCoords = [
-    new google.maps.LatLng(33.5362475, -111.9267386),
-    new google.maps.LatLng(33.5104882, -111.9627875),
-    new google.maps.LatLng(33.5004686, -111.9027061)
+    new google.maps.LatLng(54.585, 21.93789),
+    new google.maps.LatLng(51.01117, 18.93382),
+    new google.maps.LatLng(50.45947, 24.57221)
   ];
+
   // Styling & Controls
   var myPolygon = new google.maps.Polygon({
     paths: triangleCoords,
